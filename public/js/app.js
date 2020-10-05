e4d32397fff23a96134f1171fc4892be854da902
Aryaman Kohli <aryamanmartinian@gmail.com>
2020-10-06 02:40:23 +0530

    remove unnecessary console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('client side js loaded') 
+
 const weatherform= document.querySelector('form')
 const search=document.querySelector('input')
 const messageone=document.querySelector('#message1')
